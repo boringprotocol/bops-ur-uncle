8dc5a73ddc6d6e780ba7eb1b6af1596ab350e364
exoboosters <ramesh@exoboosters.tech>
2022-04-22 03:19:04 -0400

    Patchwork

---
+++ core/users.js
@@ -0,0 +1,245 @@
+//MySQL and BN libs.
+var mysql = require("promise-mysql");
+var fs = require("fs");
+var BN = require("bignumber.js");
+BN.config({
+    ROUNDING_MODE: BN.ROUND_DOWN,
+    EXPONENTIAL_AT: process.settings.coin.decimals + 1
+});
+
+//Definition of the table: `name VARCHAR(64), soladdress VARCHAR(64), balance VARCHAR(64), notify tinyint(1)`.
+
+//MySQL connection and table vars.
+var connection, table;
+
+//RAM cache of users.
+var users;
+
+//Array of every handled TX hash.
+var handled;
+
+//Checks an amount for validity.
+async function checkAmount(amount) {
+    //If the amount is invalid...
+    if (amount.isNaN()) {
+        return false;
+    }
+
+    //If the amount is less than or equal to 0...
+    if (amount.lte(0)) {
+        return false;
+    }
+
+    //Else, return true.
+    return true;
+}
+
+//Creates a new user.
+async function create(user) {
+    //If the user already exists, return.
+    if (users[user]) {
+        return false;
+    }
+    // User table is now updated to
+	// name
+	// soladdress
+	// balance
+	// notify
+	// bopaddress
+	// txid
+    //Create the new user, with a blank address, balance of 0, and the notify flag on.
+    await connection.query("INSERT INTO " + table + " VALUES(?, ?, ?, ?, ?, ?)", [user, "", "0", 1, "0", "0"]);
+    //Create the new user in the RAM cache, with a status of no address, balance of 0, the notify flag on, received amount of 0 and sent amount of 0.
+    users[user] = {
+        address: false,
+        balance: BN(0),
+        notify: true,
+		received: BN(0),
+		sent: BN(0)
+    };
+
+    //Return true on success.
+    return true;
+}
+
+//Sets an user's address.
+async function setAddress(user, address) {
+    //If they already have an address, return.
+    if (typeof(users[user].address) === "string") {
+        return;
+    }
+
+    //Update the table with the address.
+    await connection.query("UPDATE " + table + " SET address = ? WHERE name = ?", [address, user]);
+    //Update the RAM cache.
+    users[user].address = address;
+}
+
+//Adds to an user's balance.
+async function addBalance(user, amount) {
+    //Return false if the amount is invalid.
+    if (!(await checkAmount(amount))) {
+        return false;
+    }
+
+    //Add the amount to the balance.
+    var balance = users[user].balance.plus(amount);
+    var received = users[user].received.plus(amount);
+    //Convert the balance to the coin's smallest unit.
+    balance = balance.toFixed(process.settings.coin.decimals);
+    //Convert the received to the coin's smallest unit.
+    received = received.toFixed(process.settings.coin.decimals);
+    //Update the table with the new balance, as a string.
+    await connection.query("UPDATE " + table + " SET balance = ?, received = ? WHERE name = ?", [balance, received, user]);
+    //Update the RAM cache with a BN.
+    users[user].balance = BN(balance);
+    users[user].received = BN(received);
+
+    return true;
+}
+
+//Subtracts from an user's balance.
+async function subtractBalance(user, amount) {
+    //Return false if the amount is invalid.
+    if (!(await checkAmount(amount))) {
+        return false;
+    }
+
+    //Subtracts the amount from the balance.
+    var balance = users[user].balance.minus(amount);
+    var sent = users[user].sent.minus(amount);
+    //Return false if the user doesn't have enough funds to support subtracting the amount.
+    if (balance.lt(0)) {
+        return false;
+    }
+
+    //Convert the balance to the coin's smallest unit.
+    balance = balance.toFixed(process.settings.coin.decimals);
+    sent = sent.toFixed(process.settings.coin.decimals);
+    //Update the table with the new balance, as a string.
+    await connection.query("UPDATE " + table + " SET balance = ? , sent = ? WHERE name = ?", [balance, sent, user]);
+    //Update the RAM cache with a BN.
+    users[user].balance = BN(balance);
+
+    return true;
+}
+
+//Updates the notify flag.
+async function setNotified(user) {
+    //Update the table with a turned off notify flag.
+    await connection.query("UPDATE " + table + " SET notify = ? WHERE name = ?", [0, user]);
+    //Update the RAM cache.
+    users[user].notify = false;
+}
+
+//Returns an user's address.
+async function getAddress(user) {
+    return users[user].address;
+}
+
+//Returns an user's balance
+async function getBalance(user) {
+    return users[user].balance;
+}
+
+//Returns an user's notify flag.
+async function getNotify(user) {
+    return users[user].notify;
+}
+
+//Returns the top tipper in the channel.
+async function toptipper() {
+    return await connection.query("SELECT name, sent FROM " + table + " WHERE sent = (SELECT MAX(sent) FROM " + table + ")");
+}
+
+module.exports = async () => {
+    //Connects to MySQL.
+    connection = await mysql.createConnection({
+        host: "localhost",
+        database: process.settings.mysql.db,
+        user: process.settings.mysql.user,
+        password: process.settings.mysql.pass
+    });
+    //Set the table from the settings.
+    table = process.settings.mysql.tips;
+
+    //Init the RAM cache.
+    users = {};
+    //Init the handled array.
+    handled = [];
+    //Gets every row in the table.
+    var rows = await connection.query("SELECT * FROM " + table);
+    //Iterate over each row, creating an user object for each.
+    var i;
+    for (i in rows) {
+        users[rows[i].name] = {
+            //If the address is an empty string, set the value to false.
+            //This is because we test if the address is a string to see if it's already set.
+            address: (rows[i].address !== "" ? rows[i].address : false),
+            //Set the balance as a BN.
+            balance: BN(rows[i].balance),
+            received: BN(rows[i].received),
+            sent: BN(rows[i].sent),
+            //Set the notify flag based on if the DB has a value of 0 or 1 (> 0 for safety).
+            notify: (rows[i].notify > 0)
+        };
+
+        //Get this user's existing TXs.
+        var txs = await process.core.coin.getTransactions(users[rows[i].name].address);
+        //Iterate over each, and push their hashes so we don't process them again.
+        var x;
+        for (x in txs) {
+            handled.push(txs[x].txid);
+        }
+    }
+
+    //Make sure all the pools have accounts.
+    for (i in process.settings.pools) {
+        //Create an account for each. If they don't have one, this will do nothing.
+        await create(i);
+    }
+
+    //Return all the functions.
+    return {
+        create: create,
+
+        setAddress: setAddress,
+        addBalance: addBalance,
+        subtractBalance: subtractBalance,
+        setNotified: setNotified,
+
+        getAddress: getAddress,
+        getBalance: getBalance,
+        getNotify: getNotify,
+
+        toptipper: toptipper
+    };
+};
+
+//Every thirty seconds, check the TXs of each user.
+setInterval(async () => {
+    for (var user in users) {
+        //If that user doesn't have an address, continue.
+        if (users[user].address === false) {
+            continue;
+        }
+
+        //Declare the amount deposited.
+        var deposited = BN(0);
+        //Get the TXs.
+        var txs = await process.core.coin.getTransactions(users[user].address);
+
+        //Iterate over the TXs.
+        for (var i in txs) {
+            //If we haven't handled them...
+            if (handled.indexOf(txs[i].txid) === -1) {
+                //Add the TX value to the deposited amount.
+                deposited = deposited.plus(BN(txs[i].amount));
+                //Push the TX ID so we don't handle it again.
+                handled.push(txs[i].txid);
+            }
+        }
+
+        await addBalance(user, deposited);
+    }
+}, 30 * 1000);
---
+++ main.js
@@ -0,0 +1,145 @@
+//Require the path lib.
+var path = require("path");
+
+//Array of each command to its file.
+var commands;
+
+//Parses a message.
+async function parseMsg(msg) {
+    //If the command exists, hand it off.
+    if (typeof(commands[msg.text[0]]) !== "undefined") {
+        await commands[msg.text[0]](msg);
+        return;
+    }
+
+    //Else, print that the command doesn't exist.
+    msg.obj.reply("That is not a command. Run `${prefix}help` to get a list of commands or edit your last message.");
+}
+
+//Prepares, verifies, and formats a message.
+async function handleMessage(msg) {
+    //Get the numeric ID of whoever sent the message.
+    var sender = msg.author.id;
+
+	// Variables from settings file
+	var prefix = process.settings.discord.botprefix;
+
+    //Do not handle messages from itself.
+    if (sender === process.settings.discord.user) {
+        return;
+    }
+
+    //Split among spaces. Remove any empty items.
+    var text = msg.content.split(" ").filter((item) => {
+        return item !== "";
+    });
+
+    //If the start of the message, is a ping to the bot, swap it for the bot prefix.
+    if (text[0] === process.settings.discord.user) {
+        text[1] = prefix + text[1];
+        //Also remove the ping.
+        text.splice(0, 1);
+    }
+
+    //Filter the message.
+    text = text
+        .join(" ")                          //Convert it back into a string.
+        .replace(/[^\x00-\x7F]/g, "")       //Remove unicode.
+        .toLowerCase()                      //Convert it to lower case.
+        .replace(new RegExp("\r", "g"), "") //Remove any \r characters.
+        .replace(new RegExp("\n", "g"), "") //Remoce any \n characters.
+        .split(" ");                        //Split it among spaces.
+
+    //If the message's first character is not the activation symbol, return.
+    if (text.substr(0, 1) !== prefix) {
+        return;
+    }
+
+    if (
+        //Create an user if they don't have an account already.
+        //If they didn't have an account, and create returned true...
+        (await process.core.users.create(sender)) ||
+        //Or if they need to be notified...
+        (await process.core.users.getNotify(sender))
+    ) {
+        //Give them the notified warning.
+        msg.reply("By continuing to use this bot, you agree to release the creator, owners, all maintainers of the bot, and the " + process.settings.coin.symbol + " Team from any legal liability. Type in any command to begin using " + process.settings.discord.user);
+        //Mark them as notified.
+        await process.core.users.setNotified(sender);
+        return;
+    }
+
+    //Remove the activation symbol.
+    text[0] = text[0].substring(1, text[0].length);
+
+    //If the command is channel locked...
+    if (typeof(process.settings.commands[text[0]]) !== "undefined") {
+        //And this is not an approved channel...
+		//Allow commands via DM - msg.guild is null for all DM channels (individual & group)
+        if (process.settings.commands[text[0]].indexOf(msg.channel.id) === -1 && msg.guild !== null) {
+            //Print where it can be used.
+            msg.reply("That command can only be run in:\r\n<#" + process.settings.commands[text[0]].join(">\r\n<#") + ">");
+            return;
+        }
+    }
+
+    //if we made it to this point, parse the message.
+    parseMsg({
+        text: text,
+        sender: sender,
+        obj: msg
+    });
+}
+
+async function main() {
+    //Load the settings into a global var so every file has access.
+    process.settings = require("./settings.json");
+    //Load it's path separately so we can write to it without writing the path.
+    process.settingsPath = path.join(__dirname, "settings.json");
+
+    //Set the core libs to a global object, so they're accessible by commands.
+    process.core = {};
+    //Require and init the coin lib, set by the settings.
+    process.core.coin = await (require("./core/" + process.settings.coin.type.toLowerCase() + ".js"))();
+    //Require and init the users lib.
+    process.core.users = await (require("./core/users.js"))();
+
+    //Declare the commands and load them.
+	//setup bal as a duplicate of balance
+    commands = {
+        help:     require("./commands/help.js"),
+        deposit:  require("./commands/deposit.js"),
+        balance:  require("./commands/balance.js"),
+		bal:      require("./commands/balance.js"),
+        tip:      require("./commands/tip.js"),
+		splash:   require("./commands/splash.js"),
+        withdraw: require("./commands/withdraw.js"),
+        pool:     require("./commands/pool.js"),
+        giveaway: require("./commands/giveaway.js"),
+		online:   require("./commands/online.js"),
+		specs:    require("./commands/specs.js")
+    };
+
+    //Create a Discord process.client.
+    process.client = new (require("discord.js")).Client();
+
+    //Handle messages.
+    process.client.on("message", handleMessage);
+    process.client.on("messageUpdate", async (oldMsg, msg) => {
+        handleMessage(msg);
+    });
+	//Added an activity status for the bot
+	process.client.on("ready", () => {
+       process.client.user.setActivity(process.settings.discord.botActivity);
+    });
+    process.client.login(process.settings.discord.token);
+}
+
+(async () => {
+    try {
+        await main();
+    } catch(e) {
+        /*eslint no-console: ["error", {allow: ["error"]}]*/
+        console.error(e);
+    }
+})();
